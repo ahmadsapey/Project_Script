4a6e0d4b510a3978d0db0a210c934229b17c0c7c
ahmadsapey <sapeyamad.lp3i.krw24@gmail.com>
2026-08-10 00:00:54 +0700

    Perubahan Upload Konten

---
+++ sheet-config.js
@@ -0,0 +1,2 @@
+// Generated: client config for Google Sheets Apps Script endpoint
+const SHEET_ENDPOINT = 'https://script.google.com/macros/s/AKfycbwmjPNeL2MNCio4Fkob7W9AK_gM_CgILTpdWl3f2JhEVdRR58Nt9TDDijuCvikw-mwh/exec';
