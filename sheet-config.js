790a58d92e936db88fee001710862ef4efc59c2f
ahmadsapey <sapeyamad.lp3i.krw24@gmail.com>
2026-08-10 01:10:33 +0700

    .

--- sheet-config.js
+++
@@ -1,2 +0,0 @@
-// Generated: client config for Google Sheets Apps Script endpoint
-const SHEET_ENDPOINT = 'https://script.google.com/macros/s/AKfycbwmjPNeL2MNCio4Fkob7W9AK_gM_CgILTpdWl3f2JhEVdRR58Nt9TDDijuCvikw-mwh/exec';
